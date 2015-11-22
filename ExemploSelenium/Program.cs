482d1feef42be27e7abb7397390c5168a1ec6ba2
Vinicius Mussak <vinicius.mussak@outlook.com>
2015-11-22 20:48:11 -0200

    Primeiro commit

---
+++ ExemploSelenium/Program.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace ExemploSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://themoneyconverter.com/");
+
+            driver.Manage().Window.Maximize();
+
+            IWebElement valueInput = driver.FindElement(By.Id("ta"));
+            IWebElement fromInput = driver.FindElement(By.Id("df"));
+            IWebElement toInput = driver.FindElement(By.Id("dt"));
+            IWebElement resultArea = driver.FindElement(By.Id("ratebox"));
+
+            const string valorDigitado = "1",
+                         valorEsperado = "BRL/USD = 3.7056",
+                         fromValue = "USD - United States Dollar",
+                         toValue = "BRL - Brazilian Real";
+
+            valueInput.SendKeys(valorDigitado);
+
+            var selectTo = new SelectElement(fromInput);
+            var selectFrom = new SelectElement(toInput);
+
+            selectTo.SelectByText(fromValue);
+            selectFrom.SelectByText(toValue);
+
+            Thread.Sleep(900);
+
+
+            if (resultArea.GetAttribute("value") == valorEsperado)
+                Console.WriteLine("Deu certo");
+            else
+                Console.WriteLine("Não deu certo");
+
+            driver.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
